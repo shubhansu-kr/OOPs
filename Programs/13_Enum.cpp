f989a48ff03ef82838b62794adb140049a686196
shubhansu-kr <shubhansu2021@gmail.com>
2022-01-27 11:55:12 +0530

    Enum

---
+++ Programs/13_Enum.cpp
@@ -0,0 +1,56 @@
+// Enum
+
+#include <iostream>
+using namespace std;
+
+enum sem
+{
+    jan,
+    feb = 10,
+    march=2,
+    april,
+    may
+};
+
+int main()
+{
+    sem x, y, z;
+
+    x = jan;
+    y = feb;
+    z = march;
+
+    cout << x << " " << y << " " << z;
+    return 0;
+}
+
+// #include<iostream>
+// using namespace std;
+
+// main()
+// {
+// enum days{sun,mon,tues,wed,thur,fri,sat};// int a,b,c; days a,b,c;
+// days x,y;
+// x=mon;//1
+// y=wed;//3
+
+// //y=x+1;
+
+// cout<<x<<" and "<<y;
+// }
+
+// #include<iostream>
+// using namespace std;
+
+// main()
+// {
+// enum days{sun=3,mon,tues,wed=1000,thur=3,fri,sat};// int a,b,c; days a,b,c;
+// days x,y;
+// //x=1;//1 error
+// x=mon;
+// y=wed;//3
+
+// // y=x+1; error
+
+// cout<<x<<" and "<<y+1;// mon wed 1 3
+// }
