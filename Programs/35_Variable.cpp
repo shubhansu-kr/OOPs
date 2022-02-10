46d28163f0a017de6e2c216e3a6fa741fc354336
shubhansu-kr <shubhansu2021@gmail.com>
2022-02-10 11:11:40 +0530

    Variable scope

---
+++ Programs/35_Variable.cpp
@@ -0,0 +1,41 @@
+// Variable scope
+
+#include <iostream>
+using namespace std;
+
+int a = 30; // Global var (Default val is 0)
+int b;
+
+int main()
+{
+    int a = 20; // Local var for main
+    cout << a << endl;
+    cout << ::a << endl; // prints global 30
+
+    {
+        int a = 10;
+        cout << a << endl;   // local to the block
+        cout << ::a << endl; // prints global 30
+        // cout << ::(::a) << endl;  Error, Wrong syntax
+    }
+
+    {
+        cout << a << endl;   // Finds the nearest a and prints 20
+        cout << ::a << endl; // prints global 30
+        // cout << ::(::a) << endl;  Error, Wrong syntax
+    }
+
+    cout << b << endl; // prints zero
+
+    int b;
+    cout << b << endl; // garbage
+
+    int c = 21;
+    {
+        int c = 13;
+        cout << c << endl;
+        // cout << ::c << endl; Error , NO global variable named c 
+    }
+
+    return 0;
+}
