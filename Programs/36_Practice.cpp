df903b281c43c595e2d2dd1fa01fa3765c698435
shubhansu-kr <shubhansu2021@gmail.com>
2022-02-10 10:47:19 +0530

    Practice

---
+++ Programs/36_Practice.cpp
@@ -0,0 +1,35 @@
+// Practice
+
+#include <iostream>
+using namespace std;
+
+void f1();
+void f2();
+
+int a = 30;
+
+int main()
+{
+    a = 20;
+    cout << a << " ";
+    a++;
+    f1();
+    cout << a << " ";
+    f2();
+    a++;
+    cout << a << " ";
+
+    return 0;
+}
+
+void f1()
+{
+    int a = 40;
+    cout << a << " ";
+}
+
+void f2()
+{
+    a = 40 ;
+    cout << a << " ";
+}
