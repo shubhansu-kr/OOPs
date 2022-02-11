7890e18210a3fcf83dc9b4ff8bed70fd15345d0a
shubhansu-kr <shubhansu2021@gmail.com>
2022-02-11 10:59:00 +0530

    Function Overloading

---
+++ Programs/39_FunctionOverloading.cpp
@@ -0,0 +1,40 @@
+// Function overloading
+
+#include <iostream>
+using namespace std;
+
+void sum(int a, int b)
+{
+    cout << a + b << endl;
+}
+
+// Ambiguos function since a Function with 3 arguments is available
+// Throws error
+
+// void sum(int a, int b, int c = 9)
+// {
+//     cout << a + b + c;
+// }
+
+void sum(float a, int b)
+{
+    cout << a + b + 100 << endl;
+}
+
+void sum(int a, int b, int c)
+{
+    cout << a + b + c << endl;
+}
+
+int main()
+{
+    sum(3, 4);    // Sum 7
+    sum(5, 3, 7); // Sum 15
+
+    // sum(3.1, 4) ;  Throws an error
+    float a = 3.1;
+    sum(a, 4); // Sum 107.1
+
+    return 0;
+}
+
