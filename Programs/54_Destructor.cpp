b850ceb4988d99ad53ffc9860d0ecc36c3835896
shubhansu-kr <shubhansu2021@gmail.com>
2022-02-24 11:37:28 +0530

    Destructor

---
+++ Programs/54_Destructor.cpp
@@ -0,0 +1,38 @@
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
+
+// #include <iostream>
+// using namespace std;
+// class Rectangle
+// {
+// private:
+//     int length, breadth;
+
+// public:
+//     Rectangle() // def const
+//     {
+//         cout << "constructor called" << this << endl;
+//     }
+//     ~Rectangle()
+//     {
+
+//         cout << "destructor called" << this << endl;
+//     }
+// };
+
+// main()
+// {
+//     Rectangle r1; // cons called 1101
+//     {
+//         Rectangle r2; // cons called 2201
+//     }                 // dest called 2201
+
+// } // dest called 1101
