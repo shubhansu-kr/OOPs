3e35e75b956f9cb386c7aae3195025e61848f87c
shubhansu-kr <shubhansu2021@gmail.com>
2022-02-24 11:56:20 +0530

    Strings

---
+++ Programs/55_Strings.cpp
@@ -0,0 +1,36 @@
+//
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+
+//     return 0;
+// }
+
+// #include <iostream>
+// #include <string>
+// using namespace std;
+
+// int main()
+// {
+//     char name[25], title[256]; // c style
+//     string song;               // cpp style
+//     cout << "Please, enter your name: ";
+//     cin.getline(name, 25);
+//     // getline(cin,name); //error
+//     cout << "Please, enter your favourite movie: ";
+//     cin.getline(title, 256);
+//     cout << "\nPlease, enter your favourite song: ";
+//     getline(cin, song);
+//     // cin.getline (song); // error
+//     cout << name << "'s favourite movie is " << title << endl;
+//     cout << name << "'s favourite song is " << song << endl;
+
+//     char str[5] = "ABC"; // collection of chars, stored in a continous m.l and terminated by null char'\0'
+
+//     cout << str[3] << endl; // no output
+
+//     cout << str; // ABC
+// }
