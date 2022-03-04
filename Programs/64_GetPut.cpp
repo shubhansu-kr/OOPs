a9eaaa00ebbb72d9b714537fe9c3cf2df238ff05
shubhansu-kr <shubhansu2021@gmail.com>
2022-03-04 10:36:31 +0530

    file Handling :: gects and puts

---
+++ Programs/64_GetPut.cpp
@@ -0,0 +1,40 @@
+// Get and Put are used to handle characters of files
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
+
+// #include <iostream>
+// using namespace std;
+// #include <fstream>
+// int main()
+// {
+//     char ch;
+//     ofstream outfile;
+//     outfile.open("h.txt");
+//     while (ch != '.')
+//     {
+//         cin >> ch; // AbC 12E if(ch>='0' && ch<='9')
+//         outfile.put(ch);
+//     }
+//     outfile.close();
+//     ifstream infile;
+//     infile.open("h.txt");
+//     while (!infile.eof()) // while(o1) while(o1.eof()
+//     {
+//         infile.get(ch);
+//         if (ch == 'a' || ch == 'e')
+//         {
+//             cout << ch;
+//         }
+//     }
+
+//     return 0;
+// }
