e7f2d8c41c4e0daefe9d7e5933bc517613c80a72
shubhansu-kr <shubhansu2021@gmail.com>
2022-03-12 10:17:47 +0530

    Test

--- Programs/00_Test.cpp
+++ Programs/00_Test.cpp
@@ -5,8 +5,15 @@ using namespace std;
 
 int main()
 {
-    int *a;
-    // a = &1; error
-    cout << a << endl;
+    // int *a;
+    // // a = &1; error
+    // cout << a << endl;
+
+    string s = "abs";
+    cout << s << endl;
+
+    cout << s[2];
+    cout << s[0 + 2];
+    cout << s + "abc" << endl;
     return 0;
 }
