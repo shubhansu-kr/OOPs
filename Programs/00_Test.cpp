566ec4e710cb228851ac502c058e49291e3a1f76
shubhansu-kr <shubhansu2021@gmail.com>
2022-03-30 13:53:52 +0530

    test

--- Programs/00_Test.cpp
+++ Programs/00_Test.cpp
@@ -6,19 +6,21 @@ using namespace std;
 class A
 {
     int a;
+    int b;
 
 public:
     A()
     {
         a = 1000;
+        b = 19;
     }
     A(int x)
     {
         cout << "PC called" << endl;
-        cout << a << endl;
+        cout << a << " " << b << endl;
         cout << a + x << endl;
         a += x;
-        cout << a << endl;
+        cout << a << " " << b << endl;
     }
     void display()
     {
@@ -34,7 +36,7 @@ int main()
     A obj1;
     obj1.display(); // Expecting - 1000
     int count = 5;
-    obj1 = count; // operator overloaded 
+    obj1 = count; //
     obj1.display();
 
     return 0;
