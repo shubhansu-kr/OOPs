217baad086459fed8f07dba5008e628e5c5da69e
shubhansu-kr <shubhansu2021@gmail.com>
2022-04-10 12:50:21 +0530

    Test

--- Programs/00_Class.cpp
+++ Programs/00_Class.cpp
@@ -1,15 +1,72 @@
-// Declare a class for Employees 
+// CA - 2 Question 2
 
 #include <iostream>
-using namespace std ;
+using namespace std;
 
-class Employee {
-    int id ;
-    char name[30] ;
-    float ctc ; 
-} ;
+class Overloading
+{
+    int data;
 
-int main () {
-    Employee roy, agr ;
+public:
+    friend bool operator>=(Overloading &obj1, Overloading &obj2);
+    friend bool operator<(Overloading &obj1, Overloading &obj2);
+    friend bool operator&&(Overloading &obj1, Overloading &obj2);
+    void operator=(int x)
+    {
+        data = x;
+    }
+
+    void setdata(int x)
+    {
+        data = x;
+    }
+    int Display()
+    {
+        return data;
+    }
+};
+
+bool operator>=(Overloading &obj1, Overloading &obj2)
+{
+    if (obj1.data >= obj2.data)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+bool operator<(Overloading &obj1, Overloading &obj2)
+{
+    if (obj1.data < obj2.data)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+bool operator&&(Overloading &obj1, Overloading &obj2)
+{
+    if (obj1.data && obj2.data)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int main()
+{
+    Overloading O1, O2, O3, O4, O5, O6, O7;
+
+    O1.setdata(3);
+    O2.setdata(4);
+    O3.setdata(6);
+    O4.setdata(2);
+    O5.setdata(6);
+    O6.setdata(2);
+    O7.setdata(1);
+
+    O7 = ((O1 >= O2) + (O3 && O4) * (O5 < O6));
+    cout << O7.Display() << endl;
+
     return 0;
 }
