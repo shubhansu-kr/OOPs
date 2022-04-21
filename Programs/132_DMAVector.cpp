f1d9334e274d8d1d097303f77ec19c8cfebd7d3a
shubhansu-kr <shubhansu2021@gmail.com>
2022-04-21 13:45:55 +0530

    Method 2 :

--- Programs/132_DMAVector.cpp
+++ Programs/132_DMAVector.cpp
@@ -20,8 +20,31 @@ int main()
     {
         for (int j = 0; j < n; j++)
         {
-            *(arr + i * m + j) = (i+j) ;
+            *(arr + i * m + j) = (i + j);
             cout << *(arr + i * m + j) << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    // Method 2 : Using an array of arrays
+    // We use a pointer to pointer
+
+    // creating m integer pointer
+    int **A = new int *[m];
+    for (int i = 0; i < m; i++)
+    {
+        // Creating n integer element
+        A[i] = new int[n];
+    }
+
+    // Now A holds the address of the first array
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            A[i][j] = i * j;
+            cout << A[i][j] << " ";
         }
         cout << endl;
     }
