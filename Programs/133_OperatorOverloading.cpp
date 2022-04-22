33281fad841a22c5dc0c5da44065cfb9be00e4dc
shubhansu-kr <shubhansu2021@gmail.com>
2022-04-22 10:00:11 +0530

    Operator overloading

--- Programs/133_OperatorOverloading.cpp
+++ Programs/133_OperatorOverloading.cpp
@@ -10,33 +10,39 @@ class A
 public:
     A(){};
     A(int a) : data(a){};
-    A operator-(A &obj)
+    A operator-(A obj)
     {
-        A temp ;
+        A temp;
         temp.data = data - obj.data;
-        return temp ;
-    }
-    
-    A operator>(A &obj)
-    {
-        A temp ;
-        temp.data = (data > obj.data);
-        return temp ;        
+        return temp;
     }
 
-    void operator=(A &obj)
+    A operator>(A obj)
     {
-       data = obj.data;
+        A temp;
+        temp.data = (data > obj.data);
+        return temp;
     }
-    void display () {
+
+    // void operator=(A obj)
+    // {
+    //    data = obj.data;
+    // }
+    void display()
+    {
         cout << "data : " << data << endl;
     }
 };
 
 int main()
 {
-    A O1, O2(4), O3(4), O4(5);
+    A O1, O2(4), O3(4), O4(0);
     O1 = O2 > O3 - O4;
+
+    // A O6 = O3;
+    // O6.display();
+    
+    cout << endl;
     O1.display();
     return 0;
 }
